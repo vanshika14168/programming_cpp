39f6060c6143e428b07348ac7ff03f6e2d747c66
Vanshika Vishwakarma <vanshikavishwakarma13@gmail.com>
2026-03-02 20:48:58 +0530

    switch case

---
+++ switch_case.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class solution{
+    public:
+    void Which_week_day(int day){
+        switch(day){
+            case 1:
+            cout<<"Monday";
+            break;
+
+            case 2:
+            cout<<"Tuesday";
+            break;
+
+            case 3:
+            cout<<"Wednesday";
+            break;
+
+            case 4:
+            cout<<"Thursday";
+            break;
+
+            case 5:
+            cout<<"Friday";
+            break;
+
+            case 6:
+            cout<<"Saturday";
+            break;
+
+            case 7:
+            cout<<"Sunday";
+            break;
+
+            default:
+            cout<<"invalid choice:";
+        }
+    }
+};
+
+int main(){
+    
+    int day;
+    cout<<"enter the day:";
+    cin>>day;
+
+    solution S;
+    S.Which_week_day(day);
+
+    return 0;
+}
